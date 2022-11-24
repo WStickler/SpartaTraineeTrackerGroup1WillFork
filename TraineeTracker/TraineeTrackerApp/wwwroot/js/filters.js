91970bb267c90436835247381f4849f5a0632aa9
William Stickler <wstickler987@gmail.com>
2022-11-24 15:37:47 +0000

    consultant and technical skill sort by
    consultant and technical skill sort by buttons for both desc and asc complete

--- TraineeTracker/TraineeTrackerApp/wwwroot/js/filters.js
+++ TraineeTracker/TraineeTrackerApp/wwwroot/js/filters.js
@@ -42,4 +42,211 @@ function OrderByDateDescending() {
             switching = true;
         }
     }
+
+}
+
+function OrderByConsultantSkillAscending() {
+    var rows, table, switching, i, x, y, shouldSwitch
+    table = document.getElementById("table")
+    switching = true
+    while (switching) {
+        switching = false
+        rows = table.rows
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false
+            x = rows[i].querySelectorAll("div.consultantValue")[0];
+            y = rows[i + 1].querySelectorAll("div.consultantValue")[0];
+            switch (x.innerText) {
+                case "Skilled":
+                    x = 4;
+                    break;
+                case "Partially Skilled":
+                    x = 3;
+                    break;
+                case "Low Skilled":
+                    x = 2;
+                    break;
+                case "Unskilled":
+                    x = 1;
+                    break;
+            }
+            switch (y.innerText) {
+                case "Skilled":
+                    y = 4;
+                    break;
+                case "Partially Skilled":
+                    y = 3;
+                    break;
+                case "Low Skilled":
+                    y = 2;
+                    break;
+                case "Unskilled":
+                    y = 1;
+                    break;
+            }
+            if (x > y) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+
+}
+
+function OrderByConsultantSkillDescending() {
+    var rows, table, switching, i, x, y, shouldSwitch
+    table = document.getElementById("table")
+    switching = true
+    while (switching) {
+        switching = false
+        rows = table.rows
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false
+            x = rows[i].querySelectorAll("div.consultantValue")[0];
+            y = rows[i + 1].querySelectorAll("div.consultantValue")[0];
+            switch (x.innerText) {
+                case "Skilled":
+                    x = 4;
+                    break;
+                case "Partially Skilled":
+                    x = 3;
+                    break;
+                case "Low Skilled":
+                    x = 2;
+                    break;
+                case "Unskilled":
+                    x = 1;
+                    break;
+            }
+            switch (y.innerText) {
+                case "Skilled":
+                    y = 4;
+                    break;
+                case "Partially Skilled":
+                    y = 3;
+                    break;
+                case "Low Skilled":
+                    y = 2;
+                    break;
+                case "Unskilled":
+                    y = 1;
+                    break;
+            }
+            if (x < y) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
+
+function OrderByTechnicalSkillAscending() {
+    var rows, table, switching, i, x, y, shouldSwitch
+    table = document.getElementById("table")
+    switching = true
+    while (switching) {
+        switching = false
+        rows = table.rows
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false
+            x = rows[i].querySelectorAll("div.technicalValue")[0];
+            y = rows[i + 1].querySelectorAll("div.technicalValue")[0];
+            switch (x.innerText) {
+                case "Skilled":
+                    x = 4;
+                    break;
+                case "Partially Skilled":
+                    x = 3;
+                    break;
+                case "Low Skilled":
+                    x = 2;
+                    break;
+                case "Unskilled":
+                    x = 1;
+                    break;
+            }
+            switch (y.innerText) {
+                case "Skilled":
+                    y = 4;
+                    break;
+                case "Partially Skilled":
+                    y = 3;
+                    break;
+                case "Low Skilled":
+                    y = 2;
+                    break;
+                case "Unskilled":
+                    y = 1;
+                    break;
+            }
+            if (x > y) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+
+}
+
+function OrderByTechnicalSkillDescending() {
+    var rows, table, switching, i, x, y, shouldSwitch
+    table = document.getElementById("table")
+    switching = true
+    while (switching) {
+        switching = false
+        rows = table.rows
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false
+            x = rows[i].querySelectorAll("div.technicalValue")[0];
+            y = rows[i + 1].querySelectorAll("div.technicalValue")[0];
+            switch (x.innerText) {
+                case "Skilled":
+                    x = 4;
+                    break;
+                case "Partially Skilled":
+                    x = 3;
+                    break;
+                case "Low Skilled":
+                    x = 2;
+                    break;
+                case "Unskilled":
+                    x = 1;
+                    break;
+            }
+            switch (y.innerText) {
+                case "Skilled":
+                    y = 4;
+                    break;
+                case "Partially Skilled":
+                    y = 3;
+                    break;
+                case "Low Skilled":
+                    y = 2;
+                    break;
+                case "Unskilled":
+                    y = 1;
+                    break;
+            }
+            if (x < y) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
 }
